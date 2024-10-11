eb9c4ec37b186dbbea7129a32e036664e1e5a312
Razaishere <info678797@gmail.com>
2024-10-11 16:04:33 +0500

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Allow access from the local network
     port: 3000, // Optional: specify a port (default is 5173)
-  },
-   base: '/e-comm-forever/'
+  }
+   
 })
